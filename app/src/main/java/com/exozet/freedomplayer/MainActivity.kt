d3c5705f7a4a80cc1c7f010a51e50cec9497b4cf
Jan Rabe <jan.rabe@exozet.com>
2018-07-06 22:22:14 +0200

    added dependencies and sample app

--- app/src/main/java/com/exozet/freedomplayer/MainActivity.kt
+++ app/src/main/java/com/exozet/freedomplayer/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.exozet.freedomplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.exozet.sequentialimage.player.SequentialImagePlayer
+import com.exozet.threehundredsixty.player.ThreeHundredSixtyPlayer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startInteriorPlayer.setOnClickListener {
+            startThreeHundredSixtyPlayer("equirectangular.png")
+        }
+
+        startExteriorPlayer.setOnClickListener {
+            startSequentialPlayer((1 until 192).map { String.format("stabilized/out%03d.png", it) }.toList())
+        }
+    }
+
+    private fun startThreeHundredSixtyPlayer(filename: String) {
+        ThreeHundredSixtyPlayer
+                .with(this)
+                .assetFile(filename)
+                .startActivity()
+    }
+
+    fun startSequentialPlayer(list: List<String>) {
+        SequentialImagePlayer
+                .with(this)
+                // .internalStorageFiles(list)
+                .assetFiles(list)
+                // .externalStorageFiles(list)
+                // .files(list)
+                .fps(24) // default: 30
+                .playBackwards(false) // default: false
+                .autoPlay(false) // default: true
+                .zoom(true) // default: true
+                .controls(true) // default: false
+                .swipeSpeed(0.8f) // default: 1
+                .startActivity()
     }
 }
