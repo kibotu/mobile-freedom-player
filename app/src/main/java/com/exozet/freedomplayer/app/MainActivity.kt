290cef50c0d8df96306ab6f14dc47b71ef3ddbe7
Jan Rabe <jan.rabe@exozet.com>
2018-08-06 19:44:44 +0200

    updated 360 player

--- app/src/main/java/com/exozet/freedomplayer/app/MainActivity.kt
+++ app/src/main/java/com/exozet/freedomplayer/app/MainActivity.kt
@@ -15,9 +15,9 @@ class MainActivity : AppCompatActivity() {
 
         FreedomPlayerActivity.startActivity(this, Parameter(
                 startPlayer = FreedomPlayerActivity.THREE_HUNDRED_SIXTY_PLAYER,
-                // threeHundredSixtyUri = parseAssetFile("equirectangular.jpg"),
+//                 threeHundredSixtyUri = parseAssetFile("equirectangular.jpg"),
                 threeHundredSixtyUri = parseAssetFile("interior_example.jpg"),
-                // threeHundredSixtyUri = Uri.parse("https://storage.googleapis.com/preview-mobile-de/default/0001/01/b66f74bd094963f1b07b297508cfdeae1262cc7f.json"),
+//                threeHundredSixtyUri = Uri.parse("https://storage.googleapis.com/preview-mobile-de/default/0001/01/b66f74bd094963f1b07b297508cfdeae1262cc7f.json"),
                 projectionMode = ThreeHundredSixtyPlayer.PROJECTION_MODE_SPHERE,
                 interactionMode = ThreeHundredSixtyPlayer.INTERACTIVE_MODE_MOTION_WITH_TOUCH,
                 showControls = true,
