79185f156bc7f9911d81a86423606cf37263cbfb
Jan Rabe <jan.rabe@exozet.com>
2019-03-14 14:23:11 +0100

    added example for activity result

--- app/src/main/java/com/exozet/freedomplayer/app/MainActivity.kt
+++ app/src/main/java/com/exozet/freedomplayer/app/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.exozet.freedomplayer.app
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.exozet.freedomplayer.FreedomPlayerActivity
 import com.exozet.freedomplayer.Parameter
@@ -9,34 +12,54 @@ import com.exozet.threehundredsixtyplayer.ThreeHundredSixtyPlayer
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val FREEDOM_PLAYER_ACTIVITY_REQUEST_CODE = 101
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        FreedomPlayerActivity.startActivity(
-            this, Parameter(
-                startPlayer = FreedomPlayerActivity.THREE_HUNDRED_SIXTY_PLAYER,
+        val parameter = Parameter(
+            startPlayer = FreedomPlayerActivity.THREE_HUNDRED_SIXTY_PLAYER,
 //                 threeHundredSixtyUri = parseAssetFile("equirectangular.jpg"),
-                threeHundredSixtyUri = parseAssetFile("interior_example.jpg"),
+            threeHundredSixtyUri = parseAssetFile("interior_example.jpg"),
 //                threeHundredSixtyUri = Uri.parse("https://storage.googleapis.com/preview-mobile-de/default/0001/01/b66f74bd094963f1b07b297508cfdeae1262cc7f.json"),
-                projectionMode = ThreeHundredSixtyPlayer.PROJECTION_MODE_SPHERE,
-                interactionMode = ThreeHundredSixtyPlayer.INTERACTIVE_MODE_MOTION_WITH_TOUCH,
-                showControls = true,
+            projectionMode = ThreeHundredSixtyPlayer.PROJECTION_MODE_SPHERE,
+            interactionMode = ThreeHundredSixtyPlayer.INTERACTIVE_MODE_MOTION_WITH_TOUCH,
+            showControls = true,
 //                sequentialImageUris = (1 until 192).map { parseAssetFile(String.format("default/out%d.png", it)) }.toTypedArray(),
 //                sequentialImageUris = (1 until 192).map { parseAssetFile(String.format("stabilized/out%03d.png", it)) }.toTypedArray(),
-                sequentialImageUri = Uri.parse("https://storage.googleapis.com/preview-mobile-de/default/0001/14/61b8ec8b30a9ed586f89ef7c1d71e479aadfe46d.json"),
-                autoPlay = true,
-                fps = 17,
-                playBackwards = false,
-                zoomable = true,
-                translatable = true,
-                swipeSpeed = 0.8f,
-                blurLetterbox = true
-            )
+            sequentialImageUri = Uri.parse("https://storage.googleapis.com/preview-mobile-de/default/0001/14/61b8ec8b30a9ed586f89ef7c1d71e479aadfe46d.json"),
+            autoPlay = true,
+            fps = 17,
+            playBackwards = false,
+            zoomable = true,
+            translatable = true,
+            swipeSpeed = 0.8f,
+            blurLetterbox = true
         )
 
-        finish()
+        FreedomPlayerActivity.startActivityForResult(this, parameter, FREEDOM_PLAYER_ACTIVITY_REQUEST_CODE)
     }
 
     fun parseAssetFile(file: String): Uri = Uri.parse("file:///android_asset/$file")
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        Log.v(MainActivity::class.java.simpleName, "[onActivityResult] requestCode=$requestCode resultCode=$resultCode data=${data?.extras}")
+
+        if (requestCode == FREEDOM_PLAYER_ACTIVITY_REQUEST_CODE) {
+
+            when (resultCode) {
+                Activity.RESULT_CANCELED -> {
+                }
+                Activity.RESULT_OK -> {
+                    var result = data?.extras
+                    // todo trigger remove event
+                }
+            }
+        }
+    }
 }
